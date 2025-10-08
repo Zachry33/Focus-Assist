c3faae9ff69430b7d93591afa37cb9e81d2d0163
Zach Simpson <Zach33Simpson@gmail.com>
2025-10-08 18:24:34 -0400

    Removed the yt shorts nav button

---
+++ scripts/youtube.js
@@ -0,0 +1,7 @@
+//Youtube.js
+
+// Find and remove short option from nav on home page
+const shortsNav = document.querySelector('[aria-label="Shorts"]');
+if (shortsNav) {
+    shortsNav.remove();
+}
