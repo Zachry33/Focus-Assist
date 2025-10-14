e2218cb626dc7c6834e26c42136b3a1cac0a6715
Zach Simpson <Zach33Simpson@gmail.com>
2025-10-14 18:10:13 -0400

    Added addtional shorts removal and throttling

--- scripts/youtube.js
+++ scripts/youtube.js
@@ -1,36 +1,90 @@
 //Youtube.js
+var count = 0;
+let lastRan = Date.now();
+let now;
+const limit = 200;
+let timeoutID;
 
-// Find and remove short option from nav on home page
-function removeNav () {
+// Find and remove shorts content on page
+function removeShorts () {
+
+    // For shorts navigation sidebar
     const shortsNav = document.querySelector('[title="Shorts"]');
-    console.log(shortsNav);
+    console.log(++count);
     if (shortsNav) {
+        console.log("hit");
         shortsNav.remove();
+    }
+
+    // For shorts in vertical displays
+    const short = document.querySelector('[is-shorts]');
+    console.log(++count);
+    if (short) {
+        console.log("hit");
+        short.remove();
+    }
+
+    // For shorts using this remix
+    const reel = document.querySelector('[class="style-scope ytd-reel-shelf-renderer"]');
+    console.log(++count);
+    if (reel) {
+        console.log("hit");
+        reel.remove();
+    }
+
+    // For shorts
+    const shortsLockup = document.querySelector('[class="shortsLockupViewModelHost"]');
+    console.log(++count);
+    if (shortsLockup) {
+        console.log("hit");
+        const shortsLockupContainer = shortsLockup.closest('grid-shelf-view-model');
+        if (shortsLockupContainer) {
+            shortsLockupContainer.remove();
+        }
+    }
+
+    // For videos in the normal video format but are in shorts style
+    const shortsOverlay = document.querySelector('[overlay-style="SHORTS"]');
+    console.log(++count);
+    if (shortsOverlay) {
+        console.log("hit");
+        const shortsOverlayContainer = shortsOverlay.closest('ytd-video-renderer');
+        if (shortsOverlayContainer) {
+            shortsOverlayContainer.remove();
+        }
     }
 }
 
-const observer = new MutationObserver(() => {
-    removeNav();
-    removeShorts();
+const observer = new MutationObserver((mutations) => {
+    for (const mutation of mutations) {
+        if (mutation.addedNodes.length > 0) {
+            throttledRemoval();
+            break;
+        }
+    }
 });
 
+function throttledRemoval () {
+    now = Date.now();
+    if (now - lastRan > limit) {
+        lastRan = now;
+        removeShorts();
+    }
+    else {
+        clearTimeout(timeoutID);
+        timeoutID = setTimeout(() => {
+            lastRan = Date.now();
+            removeShorts();
+        }, limit - (now - lastRan));
+    }
+}
+
+
 // Start observing
-observer.observe(document.body, {
+const content = document.getElementById("content");
+observer.observe(content, {
     childList: true,
     subtree: true,
 });
 
-function removeShorts () {
-    const short = document.querySelector('[is-shorts]');
-    if (short) {
-        short.remove();
-    }
-    
-    const reel = document.querySelector('[class="style-scope ytd-reel-shelf-renderer"]')
-    if (reel) {
-        reel.remove();
-    }
-}
-
 removeShorts();
-removeNav();
